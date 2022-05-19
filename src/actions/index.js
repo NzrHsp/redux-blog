c7eccbba42e268dadaedb55969bf66b36929e88e
NzrHsp <nazar.hushpit@gmail.com>
2022-05-19 12:56:51 +0300

    add files

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import JsonPlaceholder from "../apis/JsonPlaceholder";
+
+export const fetchPosts = () => async (dispatch) => {
+  const response = await JsonPlaceholder.get("/posts");
+  dispatch({
+    type: "FETCH_POSTS",
+    payload: response.data,
+  });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await JsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
